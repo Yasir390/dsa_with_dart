7dd400299db52f3f68a693667795ba2147145e51
Yasir Arafat <yasirarafat6959@gmail.com>
2026-03-02 00:16:18 +0600

    selection sort with dart ongoing

---
+++ sorting_algo/selection_sort.dart
@@ -0,0 +1,24 @@
+void main() {
+  List<int> arr = [64, 25, 12, 22, 11];
+  selectionSort(arr);
+  print("Sorted array: $arr");
+}
+
+void selectionSort(List<int> arr) {
+  int n = arr.length;
+
+  for (int i = 0; i < n - 1; i++) {
+    int minIndex = i;
+
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap
+    int temp = arr[minIndex];
+    arr[minIndex] = arr[i];
+    arr[i] = temp;
+  }
+}
